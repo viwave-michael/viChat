aef7bc5af0d4eca4c6dfe65b3b9a9ac4dbc512be
Michael Wang <michael.icwang@gmail.com>
2015-11-28 11:15:18 +0800

    Add conversation model.

---
+++ app/models/conversation.rb
@@ -0,0 +1,12 @@
+class Conversation < ActiveRecord::Base
+  belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+
+  has_many :messages, dependent: :destroy
+
+  validates_uniqueness_of :sender_id, scope: :recipient_id
+
+  scope :involving, -> (user) do
+    where("conversations.sender_id=? OR conversations.recipient_id=?", user.id, user.id)
+  end
+end

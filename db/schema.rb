c5d8f2cc7d7d7638b5b8b4c7d404e08ac6dedc43
Michael Wang <michael.icwang@gmail.com>
2015-11-30 11:34:15 +0800

    Fix errors.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128031601) do
+ActiveRecord::Schema.define(version: 20151130033337) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151128031601) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
